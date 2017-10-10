1b5d83306c1888b6a97cdd336a7b30eaea9409ce
Mykyta Protsenko <mykyta@protsenko.com>
2017-10-09 23:24:59 -0700

    Zipkin

--- src/main/java/org/grpcsvrest/rcs/Application.java
+++ src/main/java/org/grpcsvrest/rcs/Application.java
@@ -2,6 +2,8 @@ package org.grpcsvrest.rcs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
